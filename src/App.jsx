e9845ab7366a9fb27aeba0721f97ff920906408b
unknown <pankrashovsergei@gmail.com>
2025-06-24 09:29:29 +0300

    pages

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
   return (
     <div className="wrapper">
       <Router>
+        <BrowserRouter basename="/dplm"></BrowserRouter>
         <Nav />
         <Routes>
           <Route path='/' element={<CreateOpros />} />
